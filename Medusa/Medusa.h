0a502f3d119e323254d0b76472f1eeaa5fd218aa
David Alarcon <davidalarcon@dorna.com>
2018-06-05 16:21:50 +0200

    Initial Commit

---
+++ Medusa/Medusa.h
@@ -0,0 +1,19 @@
+//
+//  Medusa.h
+//  Medusa
+//
+//  Created by David Alarcon on 05/06/2018.
+//  Copyright © 2018 David Alarcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Medusa.
+FOUNDATION_EXPORT double MedusaVersionNumber;
+
+//! Project version string for Medusa.
+FOUNDATION_EXPORT const unsigned char MedusaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Medusa/PublicHeader.h>
+
+
