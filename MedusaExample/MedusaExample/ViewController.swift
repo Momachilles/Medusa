e291e8c2bbe17b0de557b6fb9755a8339d229269
David Alarcon <momachilles@gmail.com>
2018-06-11 14:14:37 +0200

    Added Example and Workspace

---
+++ MedusaExample/MedusaExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MedusaExample
+//
+//  Created by David Alarcon on 11/06/2018.
+//  Copyright © 2018 David Alarcon. All rights reserved.
+//
+
+import UIKit
+import Medusa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
